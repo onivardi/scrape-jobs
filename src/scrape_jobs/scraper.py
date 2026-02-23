6062b77b33cc730b8088d3b841a4d5ddb77a7dbe
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2026-02-22 22:25:33 -0400

    Add job details extraction and CSV output functionality

--- src/scrape_jobs/scraper.py
+++ src/scrape_jobs/scraper.py
@@ -1,23 +1,31 @@
+import csv
 from bs4 import BeautifulSoup
 import requests
 import re
 
 def main() -> None:
-    
     base_url = "https://realpython.github.io/fake-jobs/"
-
     response = requests.get(base_url)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # Find all h2, and substite all symbols and spaces with '-'
     titles = soup.find_all("h2", class_="title")
     number_job = 0
-    for title in titles:
-        title_nav = re.sub(r'[^a-zA-Z0-9]+', '-', title.get_text().lower()).rstrip('-')
+    jobs = []
 
-        response = requests.get(f"{base_url}jobs/{title_nav}-{number_job}")
-        soup = BeautifulSoup(response.text, "html.parser")
+    with open("jobs.csv", "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Title", "Company", "Description", "Location"])
 
-        print(f"{base_url}jobs/{title_nav}-{number_job}")
-        print(soup.find("div", {'class': 'box'}))
-        number_job += 1
+        for title in titles:
+            title_nav = re.sub(r'[^a-zA-Z0-9]+', '-', title.get_text().lower()).rstrip('-')
+            response = requests.get(f"{base_url}jobs/{title_nav}-{number_job}")
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            elements_header = soup.find("div", {'class': 'box'})
+            job_title = elements_header.find("h1", class_="title")
+            company = elements_header.find("h2", class_="company")
+            description = soup.select("div.content p:not(:last-child)")
+            location = re.sub(r'Location: ', '', description[1].get_text())
+
+            writer.writerow([job_title.get_text(), company.get_text(), description[0].get_text(), location])
+            number_job += 1
